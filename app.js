ab7c40a616785a02c62d41b88a32007a08d7d49b
saorregog <saorregog@gmail.com>
2022-02-02 18:43:31 -0500

    Entrega sprint 2 NFTART

--- app.js
+++ app.js
@@ -2,26 +2,30 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.listen(3004, () =>
-    console.log('servidor corriendo')
+app.listen(process.env.PORT || 3000, () =>
+    console.log('Servidor corriendo en el puerto 3000')
 );
+
+app.get('/',(req, res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
 
 app.get('/marketplace',(req, res)=>{
     res.sendFile(__dirname + '/views/marketplace.html');
 });
 
-app.get('/',(req, res)=>{
-    res.sendFile(__dirname + '/views/marketplace.html');
+app.get('/producto',(req,res)=>{
+    res.sendFile(__dirname + '/views/producto.html')
+});
+
+app.get('/cart',(req,res)=>{
+    res.sendFile(__dirname + '/views/cart.html')
 });
 
 app.get('/usuario',(req,res)=>{
     res.sendFile(__dirname + '/views/usuario.html')
 });
 
-
 app.use(express.static(path.join(__dirname, './public')));
 
-
-
-
-
+app.use(express.static(path.join(__dirname, './views')));
