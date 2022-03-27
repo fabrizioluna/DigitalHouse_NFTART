77b02286502b40c32d14e3a9e6e67a91759ef5dc
saorregog <saorregog@gmail.com>
2022-03-26 21:42:31 -0500

    Middlewares y CRUD de usuarios

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const EXPRESS = require('express');
 const APP = EXPRESS();
 const PATH = require('path');
 const METHODOVERRIDE = require('method-override'); // Para poder usar los métodos PUT y DELETE
+const session = require('express-session');
+const cookie = require('cookie-parser');
 
 APP.use(EXPRESS.static(PATH.join(__dirname, './public')));
 APP.use(EXPRESS.urlencoded({extended: false}));
 APP.use(EXPRESS.json());
 APP.use(METHODOVERRIDE('_method')); 
+APP.use(cookie());
 
 const MAIN = require('./src/routes/mainRoutes');
 const PRODUCT = require('./src/routes/productRoutes');
@@ -19,6 +22,8 @@ APP.listen(process.env.PORT || 3000, function () {
 APP.use('/', MAIN);
 APP.use('/product', PRODUCT);
 APP.use('/user', USER);
+
+APP.use(session({secret: '4E4654415254'}));
 
 APP.use('*', function (req, res) {
     res.sendFile(__dirname + '/public/img/error_404.png')
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,30 @@
 const EXPRESS = require('express');
 const ROUTER = EXPRESS.Router();
+const { body } = require('express-validator');
+
+const validacionRegistro = [
+    body('nombreCompleto').notEmpty().withMessage('Completa el nombre'),
+    body('nombreUsuario').notEmpty().withMessage('Completa el nombre de usuario'),
+    body('email').notEmpty().withMessage('Completa el correo electrónico'),
+    body('fechaNacimiento').notEmpty().withMessage('Completa la fecha de nacimiento'),
+    body('pais').notEmpty().withMessage('Completa pais de origen'),
+    body('tipoCuenta').notEmpty().withMessage('Define el tipo de cuenta'),
+    body('contrasenia1').notEmpty().withMessage('Ingresa una contraseña'),
+    body('contrasenia2').notEmpty().withMessage('Ingresa una contraseña'),
+];
+
+const validacionLogin = [
+    body('email').notEmpty().withMessage('Completa el correo electrónico'),
+    body('contrasenia').notEmpty().withMessage('Completa la contraseña'),
+];
 
 const USER = require('../controllers/userController');
 
 ROUTER.get('/', USER.profile);
 ROUTER.get('/edicionRegistro', USER.editProfile);
+ROUTER.get('/usuarioRegistro', USER.register);
+ROUTER.post('/usuarioRegistro', validacionRegistro, USER.store);
+ROUTER.get('/usuarioLogin', USER.login);
+ROUTER.post('/usuarioLogin', validacionLogin, USER.enter);
 
 module.exports = ROUTER;
