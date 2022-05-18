9a09f5f2d644640884f35983ba82a671030a9280
Hresendizsuarez <hresendizsuarez@gmail.com>
2022-05-17 20:48:12 -0500

    ya casi casi

--- public/js/Cart/SessionCart.js
+++ public/js/Cart/SessionCart.js
@@ -7,13 +7,40 @@ console.log(cart)
     // let cart = JSON.parse(miStorage)
     // let cart2 =[...cart]
 
+const productCart = document.getElementById("productCart") 
+
     cart.map(function(e){
-        productCart.innerHTML +=  '<div>'+ e.nombre_nft+' - '+e.precio_actual_usd+'</div>'
+        productCart.innerHTML += insertproduct (e.nombre_nft, e.precio_actual_usd, e.precio_actual_eth, e.autor)
     })
     
     // cart.forEach(element => {
     //     productCart.innerHTML +=  '<div>'+ element.nombre_nft+' - '+element.precio_actual_usd+'</div>'
     // });
 
-
+    function insertproduct(nombrenft, preciousd, precioeth, autor){ 
+        //Codigo que utiliza variables dentro de texto
+        return `
+            <div class='img'>
+                <img src='     '>
+            </div>
+            <div class='information'>
+                <h3 id="nombre_nft" class='information-name'> ${nombrenft} </h3>
+                <div class='information-price'>
+                    <div id= "precio_actual_usd" class='priceUSD'>
+                        <h4> ${preciousd}</h4>
+                    </div>
+                    <div id= "precio_actual_eth" class='precio_actual_eth'>
+                        <h4> ${precioeth}</h4>
+                    </div>
+                    <div id ="autor" class='autor'>
+                    <h4> ${autor}</h4>
+                     </div>
+                    <div class='trash'>
+                        <i class="fas fa-trash-alt"></i>
+                    </div>
+                </div>
+            </div>
+        `        
+    }
+    
 
